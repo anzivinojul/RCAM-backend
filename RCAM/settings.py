f9bab758f279ee094544bc0dbbe2c04e9933ecd0
anzivinojul <anzivinojulien@gmail.com>
2021-11-15 23:02:30 +0100

    modify permissions

--- RCAM/settings.py
+++ RCAM/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'rest_framework.authtoken',
     'django_filters',
     'corsheaders',
 
@@ -57,7 +56,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
         'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
 }
 
--- recette/views.py
+++ recette/views.py
@@ -2,32 +2,39 @@ from django.db.models import query
 from django.shortcuts import render
 
 from rest_framework import generics, viewsets
+from rest_framework.permissions import AllowAny
 from .models import Recette, IngredientsRecette, PreparationRecette
 from .serializers import RecetteSerializer, IngredientsRecetteSerializer, PreparationRecetteSerializer
 
 class RecetteList(generics.ListCreateAPIView) :
     queryset = Recette.objects.all()
+    permission_classes = (AllowAny,)
     serializer_class = RecetteSerializer
 
 class RecetteDetail(generics.RetrieveUpdateDestroyAPIView) :
     queryset = Recette.objects.all()
+    permission_classes = (AllowAny,)
     serializer_class = RecetteSerializer
 
 class RecetteViewSet(viewsets.ModelViewSet) :
     queryset = Recette.objects.all()
+    permission_classes = (AllowAny,)
     serializer_class = RecetteSerializer
     search_fields = ['name']
 
 class IngredientsRecetteList(generics.ListCreateAPIView) : 
     queryset = IngredientsRecette.objects.all()
+    permission_classes = (AllowAny,)
     serializer_class = IngredientsRecetteSerializer
 
 class IngredientsRecetteDetail(generics.RetrieveUpdateDestroyAPIView) : 
     queryset = IngredientsRecette.objects.all()
+    permission_classes = (AllowAny,)
     serializer_class = IngredientsRecetteSerializer
 
 class IngredientsRecetteViewSet(viewsets.ModelViewSet) :
     queryset = IngredientsRecette.objects.all()
+    permission_classes = (AllowAny,)
     serializer_class = IngredientsRecetteSerializer
     filterset_fields = ['recette']
     
