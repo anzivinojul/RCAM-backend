aef9b392c46e2e18ec2990ccab0dbbc18dbe3c2c
anzivinojul <anzivinojulien@gmail.com>
2022-01-04 14:49:52 +0100

    fix to deploy

--- RCAM/settings.py
+++ RCAM/settings.py
@@ -113,23 +113,23 @@ WSGI_APPLICATION = 'RCAM.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config("db_name"),
-        'USER': config("db_user"),
-        'PASSWORD': config("db_password"),
-        'HOST': config("db_host"),
-        'PORT': config("db_port"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': config("db_name"),
+#        'USER': config("db_user"),
+#        'PASSWORD': config("db_password"),
+#        'HOST': config("db_host"),
+#        'PORT': config("db_port"),
+#    }
+#}
 
 MEDIA_ROOT= os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
