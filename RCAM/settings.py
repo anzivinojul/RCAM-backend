7f6a5ccc9768b4c017fb4bbaae5c62f3ccaae039
anzivinojul <anzivinojulien@gmail.com>
2022-01-04 18:27:39 +0100

    fix media bug with whitenoise when DEBUG = False

--- RCAM/settings.py
+++ RCAM/settings.py
@@ -131,9 +131,6 @@ DATABASES = {
 #    }
 #}
 
-MEDIA_ROOT= os.path.join(BASE_DIR, "media")
-MEDIA_URL = '/media/'
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -170,18 +167,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+if DEBUG:
+  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+                             
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 django_heroku.settings(locals())
 
--- RCAM/urls.py
+++ RCAM/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+
+from django.views.static import serve
 
 from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 
@@ -28,5 +31,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('auth.urls'), name="authentication"),
     path('schema/', SpectacularAPIView.as_view(), name="schema"),
-    path('docs/', SpectacularSwaggerView.as_view(url_name="schema"), name="docs")
+    path('docs/', SpectacularSwaggerView.as_view(url_name="schema"), name="docs"),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
